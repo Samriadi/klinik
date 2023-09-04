5c943192a9d77e71a21be1ca383462384af8012e
samriadi <basosamriadi01@gmail.com>
2023-09-04 23:26:21 +0800

    make dokters migration

---
+++ database/migrations/2023_09_04_062126_create_dokters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dokters', function (Blueprint $table) {
+            $table->id('id_dokter');
+            $table->string('nama_dokter');
+            $table->string('nip_dokter');
+            $table->integer('nohp_dokter');
+            $table->string('foto_dokter');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dokters');
+    }
+};
